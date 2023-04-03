ed998738cac83b1ddc6c75140833d16a4d611253
Eric Wang <ewang1997@gmail.com>
2023-04-03 12:51:54 -0700

    use askgpt in app

--- src/app.py
+++ src/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from chatbot import askgpt
 
 app = Flask(__name__)
-
 
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
+    chat_log = session.get('chat_log')
 
-    # use the incoming message to generate the response here
+    answer, chat_log = askgpt(incoming_msg, chat_log)
+    session['chat_log'] = chat_log
 
     r = MessagingResponse()
-    r.message('this is the response')
+    r.message(answer)
     return str(r)
