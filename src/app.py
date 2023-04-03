c8bf66825aae4dbc89924c1556134144c7b02d09
Eric Wang <ewang1997@gmail.com>
2023-04-03 12:55:07 -0700

    add /clear feat

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
-from chatbot import askgpt
+from chatbot import askgpt, start_chat_log
 
 app = Flask(__name__)
 
@@ -9,7 +9,12 @@ def bot():
     incoming_msg = request.values['Body']
     chat_log = session.get('chat_log')
 
-    answer, chat_log = askgpt(incoming_msg, chat_log)
+    if incoming_msg == '/clear':
+      chat_log = start_chat_log
+      answer = 'Chat log cleared.'
+    else:
+      answer, chat_log = askgpt(incoming_msg, chat_log)
+
     session['chat_log'] = chat_log
 
     r = MessagingResponse()
